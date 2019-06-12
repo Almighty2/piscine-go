ba718b7d57ef07d809b724658669ebd6853dd5f2
Almighty2 <oumssekabore2@gmail.com>
2019-06-12 16:17:20 +0100

    Modification du code isnegative

--- test/test.go
+++ test/test.go
@@ -1,17 +1,18 @@
 package main
 
-
+import "fmt"
 
 func main() {
 	IsNegative(1)
 	IsNegative(0)
 	IsNegative(-1)
 }
+
 func IsNegative(nb int) {
 
-	if nb<0 {
-		print("T\n")
-	}else {
-		print("F\n")
+	if nb < 0 {
+		fmt.print("T\n")
+	} else {
+		fmt.print("F\n")
 	}
 }
