bec07593cf0dd93436676bb897470b0272e13d43
Almighty2 <oumssekabore2@gmail.com>
2019-06-25 15:19:38 +0100

    First de Cat et quest8.txt et quest8T.txt

---
+++ cat/cat.go
@@ -0,0 +1,39 @@
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func check(e error) bool {
+	if e != nil {
+		fmt.Print(e)
+		return true
+	}
+	return false
+}
+
+func read(filename string) (string, bool) {
+	data, err := ioutil.ReadFile(filename)
+	a:=check(err)
+	return string(data), a
+}
+
+func main() {
+	filename:=os.Args
+	if len(filename)==1{
+		fmt.Print()
+	}else{
+
+		for i:=1;i<len(filename);i++{
+			data, a:=read(filename[1])
+			fmt.Println(data)
+			if a{
+				break
+			}
+		}
+
+	}
+}
