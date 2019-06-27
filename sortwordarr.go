9ec778bcc279bd37d5f9f26ac49bc0f89b8b0399
Almighty2 <oumssekabore2@gmail.com>
2019-06-27 17:41:14 +0100

    Deuxieme commit de SortWordArr

--- sortwordarr.go
+++ sortwordarr.go
@@ -1,31 +1,7 @@
 package piscine
 
-func SortWordArr(array []string) { //La fonction SORTWORDARR
-	ech:=""
-	for i:=0;i<len(array);i++{
-		for j:=i+1;j<len(array);j++{
-			a:=[]byte(array[j])
-			b:=[]byte(array[i])
-			if comparearrayascii(a, b){
-				ech=array[i]
-				array[i]=array[j]
-				array[j]=ech
-			}
-		}
-	}
-}
+import "sort"
 
-func comparearrayascii(a, b []byte)bool{
-	n:=0
-	if len(a)<=len(b){
-		n=len(a)
-	}else{
-		n=len(b)
-	}
-	for i:=0;i<n;i++{
-		if b[i]<a[i]{
-			return false
-		}
-	}
-	return true
+func SortWordArr(array []string) {
+	sort.Strings(array)
 }
