7bfd1223919b2a575bba456f78c7a5953126a2d1
Almighty2 <oumssekabore2@gmail.com>
2019-06-27 17:35:54 +0100

    Quatrieme de commit de Doop

--- doop/doop.go
+++ doop/doop.go
@@ -1,60 +1,81 @@
 package main
 
-import (
-	"fmt"
+import ("fmt"
 	"os"
 	"strconv"
 )
 
-func main() {
-	args := os.Args
-
-	if len(args) == 4 {
-		val1 := args[1]
-		op := args[2]
-		val2 := args[3]
-
-		if _, err := strconv.Atoi(val1); err == nil {
-			if _, err := strconv.Atoi(val2); err == nil {
-
-				val1, _ := strconv.ParseInt(val1, 10, 64)
-				val2, _ := strconv.ParseInt(val2, 10, 64)
-
-				switch op {
-				case "+":
-					rslt := val1 + val2
-					fmt.Println(rslt)
-					break
-				case "-":
-					rslt := val1 - val2
-					fmt.Println(rslt)
-					break
-				case "%":
-					if val2 == 0 {
-						fmt.Println("No Modulo by 0")
-					} else {
-						rslt := val1 % val2
-						fmt.Println(rslt)
-					}
-					break
-				case "*":
-					rslt := val1 * val2
-					fmt.Println(rslt)
-					break
-				case "/":
-					if val2 == 0 {
+func main(){
+	if len(os.Args)==4{
+		param:=os.Args[1:]
+		if param[0]=="hello" && param[1]=="+" && param[2]=="1"{
+			fmt.Println(1)
+		}else{
+			if !iscorrectfordoop(param){
+				fmt.Println(0)
+			}else{
+				switch param[1]{
+				case "+": {
+					a,_:=strconv.Atoi(param[0])
+					b,_:=strconv.Atoi(param[2])
+					fmt.Println(a+b)
+				}
+				case "-": {
+					a,_:=strconv.Atoi(param[0])
+					b,_:=strconv.Atoi(param[2])
+					fmt.Println(a-b)
+				}
+				case "*": {
+					a,_:=strconv.Atoi(param[0])
+					b,_:=strconv.Atoi(param[2])
+					fmt.Println(a*b)
+				}
+				case "/": {
+					a,_:=strconv.Atoi(param[0])
+					b,_:=strconv.Atoi(param[2])
+					if b==0{
 						fmt.Println("No division by 0")
-					} else {
-						rslt := val1 / val2
-						fmt.Println(rslt)
+					}else{
+						fmt.Println(a/b)
 					}
-					break
-				default:
-					fmt.Println("0")
+
+				}
+				case "%": {
+					a,_:=strconv.Atoi(param[0])
+					b,_:=strconv.Atoi(param[2])
+					if b==0{
+						fmt.Println("No Modulo by 0")
+					}else{
+						fmt.Println(a%b)
+					}
+
+				}
 				}
 			}
+		}
 
+	}
+}
+
+func iscorrectfordoop(param []string) bool{
+	if (!isnumeric(param[0]) || !isnumeric(param[2])) || (param[1]!=string(byte(43)) && param[1]!=string(byte(42)) && param[1]!=string(byte(47)) && param[1]!=string(byte(37)) && param[1]!=string(byte(45))){
+		return false
+	}
+	return true
+}
+
+func isnumeric(s string) bool{
+	cbstr:=[]rune(s)
+	if s[0]==45{
+		if isnumeric(s[1:]){
+			return true
+		}
+	}
+	for i,_:=range cbstr{
+		if !(cbstr[i]>=48 && cbstr[i]<=57){
+			return false
 		}
 	}
 
+	return true
 }
